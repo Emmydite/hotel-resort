0f16864b6d880300d94048f22c3d6e8b1fa3723f
emmysteve <emmacomessi@gmail.com>
2020-05-16 14:15:55 +0100

    added a gitingnore file for credentials

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful';
 
 export default createClient({
-    space : "sggt58s22nv2",
-    accessToken:"1tXwRpUHQj2lgC0GM01mgylf2XTlcC2qM0SUzzIF8FA"
+    space : process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
